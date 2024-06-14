8a6e0e0aa9441470763ba3b0429e699444e7c1bf
karineyasmin <karine.yasmin@outlook.com>
2024-06-14 16:57:28 -0300

    add

--- loops/iteradores.rb
+++ loops/iteradores.rb
@@ -6,6 +6,7 @@ end
 
 
 # -------------------------------------------------
+# each_pair - chave e valor
 band =
   {
     guitar: 'johnny',
@@ -18,10 +19,35 @@ band.each_pair do |key, value|
   puts "#{value} plays #key}"
 end
 # -------------------------------------------------
+# each_collect --> recupera elementos que atendem condição
+# retorna true ou false
 values = [1,2,3,4,5,6,7,8,9]
-p values.collect { |n| n % 2 == 0}
+p values.collect { |n| n % 2 == 0} # expressão unica linha
+values.collect do |n| # com lógica mais complexa
+  n % 2 == 0
+end
 
 result = values.collect do |n|
   n % 2 == 0
 end
 p result
+# -------------------------------------------------
+# select --> seleciona apenas
+# retorna os valores
+result = values.select do |n|
+  n % 2 == 0
+end
+
+p result
+# -------------------------------------------------
+# map ---->  mapeia um array para outro array
+result_map = values.map { |n| n * n }
+p result_map
+#--------------------------------------------------
+# inject --> injeta um operação entre cada par de elementos do array
+total = values.inject(0) { |sum, element| sum + element }
+puts "Total: #{total}"
+
+total = values.inject(:+) #mesma coisa que a linha de cima
+puts "Total: #{total}"
+#--------------------------------------------------
